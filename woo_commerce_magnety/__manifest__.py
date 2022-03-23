3bbaae17cd33b919041c853ec7277a22dbc1843e
Yevhen Pechurin <yevhen.pechurin@gmail.com>
2022-03-23 19:43:59 +0200

    [ADD][MAGN-0039] woo_commerce_magnety: added module

---
+++ woo_commerce_magnety/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Odoo WooCommerce Customization',
+    'version': '14.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Odoo Woocommerce Connector helps you automate your vital business processes at Odoo by enabling '
+               'bi-directional data exchange between WooCommerce & Odoo.',
+
+    'author': 'SmartTekSas',
+    'website': "http://smartteksas.com",
+    'maintainer': 'SmartTekSas',
+    'depends': [
+        'woo_commerce_ept',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
