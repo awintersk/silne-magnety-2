a3bcccc837007c33d921ba3f508cf85111486039
Andrey Torkin <andy.torkin@smartteksas.com>
2022-04-07 13:15:54 +0300

    MAGN-0043 [DEV] woo_commerce_magnety: renamed woo techinal products

--- woo_commerce_magnety/__manifest__.py
+++ woo_commerce_magnety/__manifest__.py
@@ -16,6 +16,7 @@
     'data': [
         'data/ir_actions_server_data.xml',
         'data/ir_config_parameter_data.xml',
+        'data/product_data.xml',
     ],
     'installable': True,
     'auto_install': False,
