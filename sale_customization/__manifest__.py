a199727e9ea4672226faa19762bfa534b613bf2b
Andrey Torkin <andy.torkin@smartteksas.com>
2022-04-26 16:15:39 +0300

    MAGN-0007 [DEV] sale_customizaiton: added delivery_list report

---
+++ sale_customization/__manifest__.py
@@ -0,0 +1,38 @@
+################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2019 SmartTek (<https://smartteksas.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+{
+    'name': 'Sale Customization',
+    'version': '14.0.1.0.0',
+    'category': 'Sales/Sales',
+    'author': 'SmartTek',
+    'website': 'https://www.smartteksas.com',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'report/sale_delivery_list_report.xml',
+        'report/sale_delivery_list_report_templates.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
