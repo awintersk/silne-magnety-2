dfb16655ef7a66a49e17b9c6ecd76a8b456a3815
Matus Krnac <matus@localhost.localdomain>
2022-06-01 12:37:45 +0200

    [ADD] added sk_invoice

---
+++ sk_invoice/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "sk_invoice",
+
+    'summary': """
+        Base addon to extend contact fields with legal data, and report view""",
+
+    'author': "krnac@implemento.sk",
+    'website': "https://implemento.sk",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'account'],
+
+    # always loaded
+    'data': [
+        'views/res_company.xml',
+        'views/res_partner.xml',
+        'views/report_invoice.xml',
+        'views/report_quotations.xml',
+        'views/account_move.xml',
+    ],
+}
---
+++ sk_invoice/models/account_move.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api, _
+
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    delivery_date = fields.Date(string="Delivery date", copy=False)
