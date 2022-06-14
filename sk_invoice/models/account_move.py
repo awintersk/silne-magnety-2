99ff91f7bf3484b85f6e6abe8d8bc1dbae06cc3f
Andrey Torkin <andy.torkin@smartteksas.com>
2022-06-14 18:49:20 +0300

    MAGN-0075 [DEV] sk_invoice: added default value for delivery_date

--- sk_invoice/models/account_move.py
+++ sk_invoice/models/account_move.py
@@ -2,8 +2,15 @@
 
 from odoo import models, fields, api, _
 
+
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     delivery_date = fields.Date(string="Delivery date", copy=False)
     narration_top = fields.Text()
+
+    def _post(self, soft=True):
+        res = super()._post(soft=soft)
+        for move in res.filtered('invoice_date'):
+            move.delivery_date = move.invoice_date
+        return res
